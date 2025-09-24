b2be9b0da642e52040b385339391d3e04e0928b9
Umut Can Özdemir <ozdemirumutcan01@gmail.com>
2025-09-24 13:33:33 +0300

    Update will-you-be-my-valentine.js

--- will-you-be-my-valentine.js
+++ will-you-be-my-valentine.js
@@ -1,4 +1,4 @@
-let messages = ["Please?", "Give me a chance!", "Are you sure?", "Think again!", "You might regret this!", "One last chance!", "Pretty please?", "Don’t break my heart 💔"];
+let messages = ["Lütfenn?", "Hem Eskiden Beğenmiştim!", "Benim Kadar Gülüyosun Bence Sendee?", "Bi Daha Düşün!", "Emin Misinnn!", "Bi Daha Beğenmemm!"];
 let noCount = 0;
 let noButton = document.getElementById("no");
 let yesButton = document.getElementById("yes");
@@ -22,7 +22,7 @@ function rejectLove() {
 function acceptLove() {
     document.getElementById("valentine").innerHTML = `
         <img src="https://media1.tenor.com/m/aEWN44So2ckAAAAC/kiss-kisses.gif" class="gif">
-        <div class="question">YAYAYYAYAYAYAY I LOVE YOU SO MUCH MY SWEET LITTLE KITTEN❤️</div>
+        <div class="question">KABUL EDECEĞİNİ BİLİYODUMMMMM</div>
     `;
     launchConfetti();
     startHeartRain();
